9a430d41402fb3c554e80cc356a78ec9289e1c05
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-08-23 15:07:27 +0300

    updated user agent

--- elysia/src/config.ts
+++ elysia/src/config.ts
@@ -7,7 +7,7 @@ export default {
   hostname: "0.0.0.0",
   version,
   userAgent:
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 YaBrowser/24.6.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 YaBrowser/24.7.0.0 Safari/537.36",
   logging: {
     level: LoggerLevel.INFO,
     logPath: path.join(__dirname, "..", "logs"),
