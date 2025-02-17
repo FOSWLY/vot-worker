8bdc1da04bf4414b225aeb8bac8ba79293c30b38
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-02-17 20:54:37 +0300

    chore: removed sonarjs

--- elysia/eslint.config.js
+++ elysia/eslint.config.js
@@ -1,6 +1,5 @@
 import js from "@eslint/js";
 import oxlint from "eslint-plugin-oxlint";
-import sonarjs from "eslint-plugin-sonarjs";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -10,12 +9,10 @@ export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
-  sonarjs.configs.recommended,
   {
     rules: {
       "@typescript-eslint/no-explicit-any": 0,
       "@typescript-eslint/consistent-type-definitions": 0,
-      "eslint-disable-next-line sonarjs/new-cap": 0,
     },
     languageOptions: {
       ecmaVersion: "latest",
