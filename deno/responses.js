60a95f85cbe843c03c02e4e91a841b837ddf4e43
Toil <62353659+ilyhalight@users.noreply.github.com>
2026-09-13 12:06:40 +0300

    chore: drop deno

--- deno/responses.js
+++
@@ -1,9 +0,0 @@
-import { Status } from "@oak/oak";
-
-function errorResponse(ctx, message) {
-  ctx.response.status = Status.NoContent;
-  ctx.response.body = null;
-  ctx.response.headers.append("X-Yandex-Status", message);
-}
-
-export { errorResponse };
